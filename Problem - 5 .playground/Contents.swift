2d9d773de7eac49429861436cb89bf916733aa10
hcd <huseyincandayan@Cans-MacBook-Pro.local>
2020-01-31 15:36:48 +0300

    Problem - 5 solution added.

---
+++ Problem - 5 .playground/Contents.swift
@@ -0,0 +1,35 @@
+/*
+ This problem was asked by Jane Street.
+
+ cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair. For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
+
+ Given this implementation of cons:
+
+ def cons(a, b):
+     def pair(f):
+         return f(a, b)
+     return pair
+ Implement car and cdr.
+ 
+ */
+
+
+typealias Pair = (a: Int, b:Int)
+
+func cons (_ a: Int, _ b: Int) -> Pair {
+    return Pair(a,b)
+}
+
+func car (_ pair: Pair) -> Int {
+    return pair.a
+}
+
+func cdr (_ pair: Pair) -> Int {
+    return pair.b
+}
+
+car(cons(1, 2))
+cdr(cons(1, 2))
+
+
+
